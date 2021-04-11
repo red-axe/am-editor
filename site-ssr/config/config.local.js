1a838c2f05dc541997b2bb43252bbb8c1ea42120
itellyou <owner@itellyou.com>
2021-04-12 03:17:18 +0800

    update docs

--- site-ssr/config/config.local.js
+++ site-ssr/config/config.local.js
@@ -3,6 +3,7 @@
 module.exports = appInfo => {
 	const config = (exports = {});
 	config.domain = 'http://localhost:7001';
+	config.umiServerPath = '../../../docs-dist/umi.server';
 	config.logger = {
 		level: 'NONE',
 		consoleLevel: 'DEBUG',
