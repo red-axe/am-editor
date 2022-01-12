28945948e9a3f65a6c44ec0b68cd9958f9e88e7c
yanmao <55792257+yanmao-cc@users.noreply.github.com>
2022-01-12 15:45:47 +0800

    fix: table template

--- plugins/table/src/component/template.ts
+++ plugins/table/src/component/template.ts
@@ -205,7 +205,7 @@ class Template implements TemplateInterface {
 			overflow !== false ? TABLE_OVERFLOW_CLASS_NAME : ''
 		}"><div class="${VIEWPORT_READER}"${
 			noBorder === true ? " data-table-no-border='true'" : ''
-		}>${transformCustomTags(html)}</div></div>`;
+		}>${transformCustomTags(html || '')}</div></div>`;
 	}
 }
 
