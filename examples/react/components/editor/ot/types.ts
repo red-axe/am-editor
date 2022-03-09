927122e8782b842b17c651ae06da59625ce9c2f7
黎星 <lixing@kkguan.com>
2022-03-09 17:49:51 +0800

    update(ot):type提取到types.ts文件

---
+++ examples/react/components/editor/ot/types.ts
@@ -0,0 +1,14 @@
+export type Member = {
+	name: string;
+	uuid: string;
+	color?: string;
+	avatar?: string;
+	index?: number;
+};
+
+export type ERROR = {
+	code: string;
+	level: string;
+	message: string;
+	error?: ErrorEvent;
+};
