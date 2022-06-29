cba3cb09a98764c82fe6d2cd4ce990fe3f33874a
yanmao <55792257+yanmao-cc@users.noreply.github.com>
2022-06-29 13:27:34 +0800

    refactor: delete console.log

--- packages/engine/src/change/event.ts
+++ packages/engine/src/change/event.ts
@@ -65,7 +65,6 @@ class ChangeEvent implements ChangeEventInterface {
 		// 处理中文输入法状态
 		// https://developer.mozilla.org/en-US-US/docs/Web/Events/compositionstart
 		this.onContainer('compositionstart', (event) => {
-			console.log('compositionstart');
 			if (this.engine.readonly) {
 				return;
 			}
@@ -135,7 +134,6 @@ class ChangeEvent implements ChangeEventInterface {
 		};
 
 		this.onContainer('compositionend', (e) => {
-			console.log('compositionend');
 			if (this.engine.readonly) {
 				return;
 			}
@@ -150,7 +148,6 @@ class ChangeEvent implements ChangeEventInterface {
 		//对系统工具栏操作拦截，一般针对移动端的文本上下文工具栏
 		//https://rawgit.com/w3c/input-events/v1/index.html#interface-InputEvent-Attributes
 		this.onContainer('beforeinput', (event: InputEvent) => {
-			console.log('beforeinput', event);
 			if (this.engine.readonly) return;
 			// safari 组合输入法会直接插入@字符，这里统一全部拦截输入@字符的时候再去触发@事件
 			const { change, card, node, block, list } = this.engine;
@@ -274,7 +271,6 @@ class ChangeEvent implements ChangeEventInterface {
 		});
 		let inputTimeout: NodeJS.Timeout | null = null;
 		this.onContainer('input', (e: InputEvent) => {
-			console.log('input', e);
 			if (this.engine.readonly) {
 				return;
 			}
