5fe783d85fe919f0be5ee3e06c50db70d3e66f33
yanmao <me@aomao.com>
2022-11-30 23:04:45 +0800

    refactor: add editable attr to td on view mode

--- plugins/table/src/component/template.ts
+++ plugins/table/src/component/template.ts
@@ -205,6 +205,9 @@ class Template implements TemplateInterface {
 	}
 
 	htmlView({ html, noBorder, overflow }: TableValue) {
+		html = transformCustomTags(html ?? '');
+		const table = $(html);
+		table.find('td').attributes(DATA_ELEMENT, EDITABLE);
 		return `<div class="${TABLE_WRAPPER_CLASS_NAME} ${
 			overflow !== false ? TABLE_OVERFLOW_CLASS_NAME : ''
 		}"><div class="${VIEWPORT_READER}"${
