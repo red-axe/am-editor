37859f685f5f42ca6f18d1907309088bfbae7811
Beeant <huangxb0512@gmail.com>
2023-03-25 07:46:47 +0000

    feat: merge upstream master

--- examples/react/components/editor/collaborators.tsx
+++ examples/react/components/editor/collaborators.tsx
@@ -6,89 +6,88 @@ import { isMobile } from '@aomao/engine';
 import { Popover, Tooltip } from 'antd';
 
 const maxCount = isMobile ? 2 : 5;
-export const Collaboration: React.FC<{
-	members: Record<number, CursorData>;
-}> = ({ members }) => {
-	const count = Object.keys(members).length;
-	const renderList = () => {
-		if (!maxCount) return;
-		const moreMembers: CursorData[] = [];
-		let count = 0;
-		for (const key in members) {
-			if (count >= maxCount) break;
-			moreMembers.push(members[key]);
-			count++;
-		}
+export const Collaboration: React.FC<{ members: Record<number, CursorData> }> =
+	({ members }) => {
+		const count = Object.keys(members).length;
+		const renderList = () => {
+			if (!maxCount) return;
+			const moreMembers: CursorData[] = [];
+			let count = 0;
+			for (const key in members) {
+				if (count >= maxCount) break;
+				moreMembers.push(members[key]);
+				count++;
+			}
+
+			return (
+				<div style={{ maxHeight: 'calc(60vh)', overflowY: 'scroll' }}>
+					{moreMembers.map(({ name, color, avatar }, index) => (
+						<div
+							key={index}
+							style={{
+								display: 'flex',
+								alignItems: 'center',
+								marginBottom: 4,
+							}}
+						>
+							<Avatar
+								size={24}
+								style={{ backgroundColor: color }}
+								src={avatar}
+							>
+								{name}
+							</Avatar>
+							<span style={{ marginLeft: '4px' }}>{name}</span>
+						</div>
+					))}
+				</div>
+			);
+		};
+
+		const renderMore = () => {
+			if (maxCount && count > maxCount) {
+				return (
+					<Popover
+						content={renderList()}
+						placement="bottomLeft"
+						overlayClassName="editor-ot-more-popover"
+						trigger={isMobile ? 'click' : 'hover'}
+					>
+						<div className="editor-ot-more-button">
+							<Avatar
+								style={{ backgroundColor: '#dbdbdb' }}
+								size={isMobile ? 24 : 30}
+							>
+								+{count - maxCount}
+							</Avatar>
+						</div>
+					</Popover>
+				);
+			}
+			return;
+		};
 
 		return (
-			<div style={{ maxHeight: 'calc(60vh)', overflowY: 'scroll' }}>
-				{moreMembers.map(({ name, color, avatar }, index) => (
-					<div
-						key={index}
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-							marginBottom: 4,
-						}}
-					>
-						<Avatar
-							size={24}
-							style={{ backgroundColor: color }}
-							src={avatar}
-						>
-							{name}
-						</Avatar>
-						<span style={{ marginLeft: '4px' }}>{name}</span>
-					</div>
-				))}
+			<div className="editor-ot-users">
+				<Space className="editor-ot-users-content" size="small">
+					{Object.values(members).map(
+						({ name, color, avatar }, index) => {
+							if (maxCount && index >= maxCount) return;
+							return (
+								<Tooltip key={index} title={name}>
+									<Avatar
+										size={isMobile ? 24 : 30}
+										style={{ backgroundColor: color }}
+										src={avatar}
+									>
+										{name}
+									</Avatar>
+								</Tooltip>
+							);
+						},
+					)}
+					{renderMore()}
+				</Space>
 			</div>
 		);
 	};
-
-	const renderMore = () => {
-		if (maxCount && count > maxCount) {
-			return (
-				<Popover
-					content={renderList()}
-					placement="bottomLeft"
-					overlayClassName="editor-ot-more-popover"
-					trigger={isMobile ? 'click' : 'hover'}
-				>
-					<div className="editor-ot-more-button">
-						<Avatar
-							style={{ backgroundColor: '#dbdbdb' }}
-							size={isMobile ? 24 : 30}
-						>
-							+{count - maxCount}
-						</Avatar>
-					</div>
-				</Popover>
-			);
-		}
-		return;
-	};
-
-	return (
-		<div className="editor-ot-users">
-			<Space className="editor-ot-users-content" size="small">
-				{Object.values(members).map(
-					({ name, color, avatar }, index) => {
-						if (maxCount && index >= maxCount) return;
-						return (
-							<Tooltip key={index} title={name}>
-								<Avatar
-									size={isMobile ? 24 : 30}
-									style={{ backgroundColor: color }}
-									src={avatar}
-								>
-									{name}
-								</Avatar>
-							</Tooltip>
-						);
-					},
-				)}
-				{renderMore()}
-			</Space>
-		</div>
-	);
-};
