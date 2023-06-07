0b4cfef5ab64728165e8e435054629d4c009431b
Yi Yang <me@aomao.com>
2023-06-08 00:43:05 +0800

    fix: yjs-protocols not found

--- plugins/yjs-protocols/.fatherrc.ts
+++ plugins/yjs-protocols/.fatherrc.ts
@@ -1,6 +1,6 @@
 export default {
 	esm: 'rollup',
 	cjs: 'rollup',
-	entry: ['src/sync.ts', 'src/awareness.ts', 'src/auth.ts'],
+	entry: ['src/index.ts', 'src/sync.ts', 'src/awareness.ts', 'src/auth.ts'],
 	runtimeHelpers: true,
 };
---
+++ plugins/yjs-protocols/src/index.ts
@@ -0,0 +1,3 @@
+export * from './auth';
+export * from './awareness';
+export * from './sync';
